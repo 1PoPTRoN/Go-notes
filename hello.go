d9aea1781c52877b11ff8b2f40f7685bf351dd7f
1PoPTRoN <vrxn.arp1traj@gmail.com>
2026-04-19 15:51:04 +0530

    dsfs
    Signed-off-by: 1PoPTRoN <vrxn.arp1traj@gmail.com>

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var age int
+	fmt.Print("Enter your name: ")
+	fmt.Scanf("%s", &name)
+	fmt.Print("Enter your age: ")
+	fmt.Scanf("%d", &age)
+	fmt.Printf("Hello, %s! You are %d years old.\n", name, age)
+}
